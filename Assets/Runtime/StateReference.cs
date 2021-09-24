d87faceacc8f7a3bf13098668e384870b74b9db0
Yevhenii Vitiuk <Yevhenii.Vitiuk@gmail.com>
2021-09-24 12:31:44 +0300

    Added stateless implementation

---
+++ Assets/Runtime/StateReference.cs
@@ -0,0 +1,10 @@
+﻿namespace Fp.Utility.Stateless
+{
+    public partial class StateMachine<TState, TTrigger>
+    {
+        internal class StateReference
+        {
+            public TState State { get; set; }
+        }
+    }
+}
---
+++ Assets/Runtime/StateRepresentationResources.cs
@@ -0,0 +1,8 @@
+namespace Fp.Utility.Stateless
+{
+    public static class StateRepresentationResources
+    {
+        public static string MultipleTransitionsPermitted =>
+            @"Multiple permitted exit transitions are configured from state '{1}' for trigger '{0}'. Guard clauses must be mutually exclusive.";
+    }
+}
